8fdc31dfc2a660280e81b37a4328f99c6c865f30
Alexander <Alexander@alexander-PC.lan>
2020-03-24 00:10:23 -0400

    Implemented placeholder methods

--- src/project/BlackJack.java
+++ src/project/BlackJack.java
@@ -1,4 +1,18 @@
 package project;
 
 public class BlackJack extends Game {
+
+    public BlackJack() {
+        super("BlackJack");
+    }
+
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void declareWinner() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
