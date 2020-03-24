31e0d807f875c0523df4dc1d3fc3c80f5ce1c0d9
Alexander <Alexander@alexander-PC.lan>
2020-03-24 01:26:13 -0400

    UML Class Diagram updated Additional Classes Generated Use Cases started

---
+++ src/project/Dealer.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Dealer extends Player {
+
+    public Dealer() {
+        super("Dealer");
+    }
+
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
