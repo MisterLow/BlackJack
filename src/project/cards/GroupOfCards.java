03b810e30f274517dc5247d585d8e5d7264b6b20
Alexander <Alexander@alexander-PC.lan>
2020-03-27 21:41:13 -0400

    Added a main method Updated classes in the Cards package

--- src/project/cards/GroupOfCards.java
+++ src/project/cards/GroupOfCards.java
@@ -2,8 +2,6 @@ package project.cards;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * A concrete class that represents any grouping of cards for a Game.
@@ -40,7 +38,7 @@ public class GroupOfCards {
     /**
      * @return the size of the group of cards
      */
-    public int getSize(){
+    public int getSize() {
         return this.cards.size();
     }
 
@@ -50,30 +48,44 @@ public class GroupOfCards {
     private void setSize(int givenSize) {
         size = givenSize;
     }
-        
+
     /**
      * Move a card from the first place in this Grouping to another Grouping
-     * @param drawLocation The GroupOfCards that you wish to move a Card into
+     *
+     * @param newLocation The GroupOfCards that you wish to move a Card into
      */
-    public void moveCard(GroupOfCards drawLocation){
-        moveCard(drawLocation,0);
+    public void moveCard(GroupOfCards newLocation) {
+        moveCard(newLocation, 0);
     }
-    
+
     /**
      * Move a card from this Grouping to another Grouping
-     * @param drawLocation The GroupOfCards that you wish to move a Card into
-     * @param cardLocation The location in the current GroupOfCards that you 
-     *          wish to remove the card from.
+     *
+     * @param newLocation The GroupOfCards that you wish to move a Card into
+     * @param cardLocation The location in the current GroupOfCards that you
+     * wish to remove the card from.
      */
-    public void moveCard(GroupOfCards drawLocation,int cardLocation){
-        drawLocation.cards.add(this.cards.get(cardLocation));
+    public void moveCard(GroupOfCards newLocation, int cardLocation) {
+        newLocation.cards.add(this.cards.get(cardLocation));
         this.cards.remove(cardLocation);
     }
-    
+
     /**
-     * 
+     * Move cards from the beginning of the Grouping to another Grouping
+     *
+     * @param amount The amount of cards you wish to move
+     * @param newLocation The GroupOfCards that you wish to move a Card into
      */
-    public void clearCards(){
+    public void moveCards(GroupOfCards newLocation,int amount) {
+        for (int i = 0; i < amount; i++) {
+            moveCard(newLocation);
+        }
+    }
+
+    /**
+     *
+     */
+    public void clearCards() {
         this.cards.clear();
     }
-}//end class
+}
